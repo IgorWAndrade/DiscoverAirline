7fe189bbf535a9540e4d6fd64d22e55165cd2c6e
Igor Andrade <igor.andrade@take.net>
2021-08-24 20:43:40 -0300

    Change service Security
    -Remove Identity.API
    -Add Security.API

--- src/DiscoverAirline/Security/DiscoverAirline.Security.API/Program.cs
+++ src/DiscoverAirline/Security/DiscoverAirline.Security.API/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DiscoverAirline.Identity.API
+namespace DiscoverAirline.Security.API
 {
     public class Program
     {
--- src/DiscoverAirline/Security/DiscoverAirline.Security.API/Startup.cs
+++ src/DiscoverAirline/Security/DiscoverAirline.Security.API/Startup.cs
@@ -1,18 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DiscoverAirline.Identity.API
+namespace DiscoverAirline.Security.API
 {
     public class Startup
     {
@@ -26,11 +20,13 @@ namespace DiscoverAirline.Identity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
 
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DiscoverAirline.Identity.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DiscoverAirline.Security.API", Version = "v1" });
             });
         }
 
@@ -41,12 +37,14 @@ namespace DiscoverAirline.Identity.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiscoverAirline.Identity.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiscoverAirline.Security.API v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
