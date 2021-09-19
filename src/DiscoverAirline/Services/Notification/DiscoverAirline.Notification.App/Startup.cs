c35006c11d410a6376e1e9d04705d8f351dc9fb5
Igor Andrade <igor.andrade@take.net>
2021-09-19 19:16:04 -0300

    Configure Startup Notificattion.App with CoreAPI

--- src/DiscoverAirline/Services/Notification/DiscoverAirline.Notification.App/Startup.cs
+++ src/DiscoverAirline/Services/Notification/DiscoverAirline.Notification.App/Startup.cs
@@ -1,40 +1,33 @@
+using DiscoverAirline.CoreAPI.Extensions;
+using DiscoverAirline.CoreAPI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DiscoverAirline.Notification.App
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApiServices(Configuration);
+
+            services.AddDocumentationServices(DocumentationSettings.Create());
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDocumentation(env);
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseApi(env);
         }
     }
 }
