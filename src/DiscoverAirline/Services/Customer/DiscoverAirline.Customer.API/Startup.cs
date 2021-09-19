9f0250bb407454dfb0d874e83a6a491db7477681
Igor Andrade <igor.andrade@take.net>
2021-09-19 19:01:59 -0300

    Configure Startup Employee.API with CoreAPI

--- src/DiscoverAirline/Services/Customer/DiscoverAirline.Customer.API/Startup.cs
+++ src/DiscoverAirline/Services/Customer/DiscoverAirline.Customer.API/Startup.cs
@@ -2,15 +2,8 @@ using DiscoverAirline.CoreAPI.Extensions;
 using DiscoverAirline.CoreAPI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DiscoverAirline.Customer.API
 {
--- src/DiscoverAirline/Services/Employee/DiscoverAirline.Employee.API/Startup.cs
+++ src/DiscoverAirline/Services/Employee/DiscoverAirline.Employee.API/Startup.cs
@@ -1,14 +1,9 @@
+using DiscoverAirline.CoreAPI.Extensions;
+using DiscoverAirline.CoreAPI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DiscoverAirline.Employee.API
 {
@@ -21,29 +16,18 @@ namespace DiscoverAirline.Employee.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiServices(Configuration);
 
-            services.AddControllers();
+            services.AddDocumentationServices(DocumentationSettings.Create());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDocumentation(env);
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApi(env);
         }
     }
 }
