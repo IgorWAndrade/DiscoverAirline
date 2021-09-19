bf8adfd5e7376f4a266309a76c3cb3b5fb652c38
Igor Andrade <igor.andrade@take.net>
2021-09-19 19:16:36 -0300

    Configure Startup Gateway.API with CoreAPI

--- src/DiscoverAirline/Gateway/DiscoverAirline.Gateway.API/Startup.cs
+++ src/DiscoverAirline/Gateway/DiscoverAirline.Gateway.API/Startup.cs
@@ -1,16 +1,9 @@
+using DiscoverAirline.CoreAPI.Extensions;
+using DiscoverAirline.CoreAPI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DiscoverAirline.Gateway.API
 {
@@ -23,37 +16,18 @@ namespace DiscoverAirline.Gateway.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiServices(Configuration);
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DiscoverAirline.Gateway.API", Version = "v1" });
-            });
+            services.AddDocumentationServices(DocumentationSettings.Create());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiscoverAirline.Gateway.API v1"));
-            }
+            app.UseDocumentation(env);
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApi(env);
         }
     }
 }
