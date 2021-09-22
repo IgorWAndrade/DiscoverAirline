c6b0226bfe3719c6ccc54e41e1c7f79f86437925
Igor Andrade <igor.andrade@take.net>
2021-09-22 14:04:29 -0300

    Implementation Log and Setup in Report.API
    - Implementation Setup on docker-compose
    - Implementation Serilog and Log Extensions

--- src/DiscoverAirline/Services/Report/DiscoverAirline.Report.App/Program.cs
+++ src/DiscoverAirline/Services/Report/DiscoverAirline.Report.App/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace DiscoverAirline.Report.App
 {
@@ -15,6 +16,7 @@ namespace DiscoverAirline.Report.App
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
