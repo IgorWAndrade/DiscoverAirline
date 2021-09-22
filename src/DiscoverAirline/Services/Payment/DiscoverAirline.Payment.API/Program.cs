e925caa346d5e86af7bd9ee1133943f64d3bd023
Igor Andrade <igor.andrade@take.net>
2021-09-22 13:57:54 -0300

    Implementation Log and Setup in Payment.API
    - Implementation Setup on docker-compose
    - Implementation Serilog and Log Extensions

--- src/DiscoverAirline/Services/Payment/DiscoverAirline.Payment.API/Program.cs
+++ src/DiscoverAirline/Services/Payment/DiscoverAirline.Payment.API/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace DiscoverAirline.Payment.API
 {
@@ -21,6 +16,7 @@ namespace DiscoverAirline.Payment.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
