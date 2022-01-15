7e3ac10f81bb9ac1dde2f2cbd8ac9e6a97045af0
Igor Andrade <33557880+IgorWAndrade@users.noreply.github.com>
2022-01-15 12:42:54 -0300

    Add RoleController
    Add RoleController and RoleViewModel

---
+++ src/DiscoverAirline/Security/DiscoverAirline.Security.API/Application/ViewModels/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscoverAirline.Security.API.Application.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string NormalizedName { get; set; } = "";
+    }
+}
